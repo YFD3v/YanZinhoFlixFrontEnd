62903c023362376bad8e8290142bf622469a9317
Yan Ferreira <fabioyan2@gmail.com>
2024-01-23 15:56:54 -0300

    Criando o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
 import styles from "./HomeNoAuth.module.scss";
 
 //Passo 1 - Configuração do projeto
 
 const HomeNoAuth = () => {
-  return <></>;
+  return (
+    <>
+      {/*Passo 4 - Criando o header parte 1 */}
+      <HeaderNoAuth />
+    </>
+  );
 };
 export default HomeNoAuth;
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Button, Container } from "reactstrap";
+import styles from "./styles.module.scss";
+import Link from "next/link";
+//Passo 4 - Criando o Header
+
+const HeaderNoAuth = () => {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        {/* o Src esta definindo a pasta public como padrão por isso está uma url curta */}
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Cadastre-se para ter acesso aos cursos</p>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          src="/logoOnebitflix.svg"
+          alt="Logo OneBitFlix"
+          className={styles.imgLogoNav}
+        />
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button className={styles.navBtn} outline>
+              Faça parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+export default HeaderNoAuth;
