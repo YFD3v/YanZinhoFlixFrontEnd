a05470c2bfa977e87281bf94b92bfe5c81d55ae1
Yan Ferreira <fabioyan2@gmail.com>
2024-01-23 15:31:09 -0300

    Configurando pastas e componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import Head from "next/head";
 import styles from "./HomeNoAuth.module.scss";
 
 //Passo 1 - Configuração do projeto
 
 const HomeNoAuth = () => {
-  return (
-    <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <main>aaa</main>
-    </>
-  );
+  return <></>;
 };
 export default HomeNoAuth;
