a88a60d6b7832df05245b3592e0172d904b91382
Yan Ferreira <fabioyan2@gmail.com>
2024-01-23 14:34:17 -0300

    Configuração do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "./HomeNoAuth.module.scss";
+
+//Passo 1 - Configuração do projeto
+
+const HomeNoAuth = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main>aaa</main>
+    </>
+  );
+};
+export default HomeNoAuth;
