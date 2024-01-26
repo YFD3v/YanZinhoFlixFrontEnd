1e2e66fc602b33eaac9f2d9411fcbd0e301534a3
Yan Ferreira <fabioyan2@gmail.com>
2024-01-26 15:57:55 -0300

    Criando a conexão do backend para login

--- src/components/Login/FormLogin/index.tsx
+++ src/components/Login/FormLogin/index.tsx
@@ -2,15 +2,63 @@
 import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "./styles.module.scss";
 import Footer from "@/components/common/Footer";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+import ToastComponent from "@/components/common/Toast";
+import authService from "@/services/authService";
 
 //Passo 17 - Estrutura da pagina de login
 
 const FormLogin = () => {
+  //Passo 18 - conexão do backend para login
+  const router = useRouter();
+  const [toast, setToast] = useState({ isOpen: false, color: "", message: "" });
+
+  //Pegando o parametro da query registred que vem da pagina de registro
+  const query = useSearchParams();
+  const registerSuccess = query.get("registred");
+  console.log(query.values);
+
+  useEffect(() => {
+    if (registerSuccess === "true") {
+      setToast({
+        message: "Cadastro realizado com sucesso!",
+        isOpen: true,
+        color: "bg-success",
+      });
+      setTimeout(() => {
+        setToast({ ...toast, isOpen: false });
+      }, 1000 * 3);
+    }
+  }, [registerSuccess]);
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+    const params = { email, password };
+
+    const { status } = await authService.login(params);
+    if (status === 200) {
+      router.push("/home");
+    } else {
+      setToast({
+        message: "E-mail ou senha incorretos!",
+        isOpen: true,
+        color: "bg-danger",
+      });
+      setTimeout(() => {
+        setToast({ ...toast, isOpen: false });
+      }, 1000 * 3);
+    }
+  };
+
   return (
     <>
       <Container className={`${styles.container} py-5`}>
         <p className={styles.formTitle}>Bem vindo(a) de volta!</p>
-        <Form className={styles.form}>
+        <Form onSubmit={handleLogin} className={styles.form}>
           <p className="text-center">
             <strong>Faça o login!</strong>
           </p>
@@ -40,11 +88,16 @@ const FormLogin = () => {
               className={styles.input}
             ></Input>
           </FormGroup>
-          <Button outline className={styles.formBtn}>
+          <Button type="submit" outline className={styles.formBtn}>
             ENTRAR
           </Button>
         </Form>
       </Container>
+      <ToastComponent
+        color={toast.color}
+        isOpen={toast.isOpen}
+        message={toast.message}
+      />
       <Footer />
     </>
   );
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -11,6 +11,11 @@ interface RegisterParams {
   password: string;
 }
 
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
 const authService = {
   register: async (params: RegisterParams) => {
     const res = await api.post("/auth/register", params).catch((err) => {
@@ -18,6 +23,18 @@ const authService = {
       if (err.response.status === 400) return err.response;
       return err;
     });
+    return res;
+  },
+  //Passo 18 - Conexão do backend para login
+  login: async (params: LoginParams) => {
+    const res = await api.post("/auth/login", params).catch((err) => {
+      if (err.response.status === 400 || err.response.status === 401)
+        return err.response;
+      return err;
+    });
+    if (res.status === 200) {
+      sessionStorage.setItem("yanzinhoflix-token", res.data.token);
+    }
     return res;
   },
 };
