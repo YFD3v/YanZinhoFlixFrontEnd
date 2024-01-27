d8310032fdee5b156f1c58a553a03c8824cddd0a
Yan Ferreira <fabioyan2@gmail.com>
2024-01-27 15:52:38 -0300

    Criação da seção de slides de lançamento

---
+++ src/components/HomeAuth/NewestCategory/index.tsx
@@ -0,0 +1,25 @@
+import SlideComponent from "@/components/common/SlideComponent";
+import courseService from "@/services/courseService";
+import useSWR from "swr";
+import styles from "../styles.module.scss";
+//Passo 21 - criação da seção de slides de lançamento
+const NewestCategory = () => {
+  //Esse swr é um facilitador para fazer fetch no backEnd
+
+  const { data, error } = useSWR("/newest", courseService.getNewestCourses);
+  if (error) return error;
+  if (!data)
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+  return (
+    <>
+      <p className={styles.titleCategory}>LANÇAMENTOS</p>
+      <SlideComponent courses={data.data} />
+    </>
+  );
+};
+
+export default NewestCategory;
--- src/components/HomeAuth/index.tsx
+++ src/components/HomeAuth/index.tsx
@@ -1,12 +1,15 @@
 //Passo 19 - criando a estrutura da página homeauth
 "use client";
+
 import HeaderAuth from "../common/HeaderAuth";
 import FeaturedSection from "./FeaturedSection";
+import NewestCategory from "./NewestCategory";
 
 const ContainerHomeAuth = () => {
   return (
     <>
       <FeaturedSection />
+      <NewestCategory />
     </>
   );
 };
